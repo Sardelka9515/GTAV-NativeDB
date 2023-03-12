ade8a4adcaf0653cc79675a7babae3d3c467d361
Sardelka9515 <sardelka9515@gmail.com>
2023-03-12 19:16:52 +0800

    Update source

--- src/external/alloc8or-nativedb/index.ts
+++ src/external/alloc8or-nativedb/index.ts
@@ -4,7 +4,7 @@ import { Alloc8orNativeJson } from './model'
 function getUrl() {
   switch (getGame()) {
     case 'GTA5':
-      return 'https://raw.githubusercontent.com/alloc8or/gta5-nativedb-data/master/natives.json'
+      return 'https://raw.githubusercontent.com/Sardelka9515/gta5-nativedb-data/master/natives.json'
     case 'RDR3':
       return 'https://raw.githubusercontent.com/alloc8or/rdr3-nativedb-data/master/natives.json'
   }
